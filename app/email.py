039fb3a846b2296e6be0340cba2336341b96ff13
VopilinS <vopilin97@inbox.ru>
2021-04-15 17:08:43 +0300

    7.2

--- app/email.py
+++ app/email.py
@@ -1,11 +1,15 @@
-def send_async_email(app, msg):
-    with app.app_context():
+from flask_mail import Mail, Message
+from threading import Thread
+from flask import render_template,current_app
+
+def send_async_email(current_app, msg):
+    with current_app.app_context():
         mail.send(msg)
 
 def send_email(to,subject, template, **kwargs):
-    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, sender=app.config['FLASKY_MAIL_SENDER'], recipients= [to])
+    msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, sender=current_app.config['FLASKY_MAIL_SENDER'], recipients= [to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
+    thr = Thread(target=send_async_email, args=[current_app, msg])
     thr.start()
     return thr
--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, current_app
 from . import main
 from .forms import NameForm
 from .. import db
 from ..models import User
+from ..email import send_email
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -14,11 +15,11 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
-            if app.config['FLASKY_ADMIN']:
-                send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail / new_user', user=user)
+            if current_app.config['FLASKY_ADMIN']:
+                send_email(current_app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
         else:
             session['known'] = True
-            session['name'] = form.name.data
-            form.name.data = ''
-            return redirect(url_for('.index'))
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('.index'))
     return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False), current_time=datetime.utcnow())
