25589f9b7ecf438fa5c4c1aaf8ed3a321e7693fd
VopilinS <vopilin97@inbox.ru>
2021-04-28 04:11:07 +0300

    8

--- app/email.py
+++ app/email.py
@@ -1,18 +1,18 @@
-from flask_mail import Mail, Message
 from threading import Thread
-from app import create_app
-from flask import render_template,current_app
-import os
+from flask import current_app, render_template
+from flask_mail import Message
 from . import mail
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
-def send_email(to,subject, template, **kwargs):
-    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, sender=app.config['FLASKY_MAIL_SENDER'], recipients= [to])
+
+def send_email(to, subject, template, **kwargs):
+    app = current_app._get_current_object()
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
     thr = Thread(target=send_async_email, args=[app, msg])
